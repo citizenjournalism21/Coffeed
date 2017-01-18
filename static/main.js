571df549af672b72ea4284ed4999ab94a6d88ca9
Monam <citizenjournalism21@gmail.com>
2017-01-18 15:26:34 +0500

    heroku1

--- static/main.js
+++ static/main.js
@@ -54,4 +54,21 @@ $("document").ready(function($){
         $('html').css('overflow-y','auto');
         $('html').css('overflow-x','hidden');
     });
+
+    $('.btn-primary').click(function(){
+        debugger;
+        $.ajaxSetup({ cache: true });
+        $.getScript('https://connect.facebook.net/en_US/sdk.js', function(){
+          FB.init({
+            appId: '1810040475914387',
+            version: 'v2.7'
+          });
+          FB.login(function(response){
+          // Handle the response object, like in statusChangeCallback() in our demo
+          // code.
+        });
+          // alert('here it is');
+        });
+
+  });
  });
